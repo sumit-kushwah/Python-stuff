fe9b7a9c4ffae8c47cdaa82f133b2f58a2d15217
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-11-05 23:29:19 +0530

    positional arguments

--- argparse/prog.py
+++ argparse/prog.py
@@ -1,7 +1,10 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator') 
 parser.add_argument('--sum', dest='accumulate', action='store_const', const=sum, default=max, help='sum the integers (default: find the max)')
-args = parser.parse_args()
+# we can also pass our list of arguments to parse_args funciton
+# by default it takes list from sys.argv
+args = parser.parse_args() 
 print(args.accumulate(args.integers))
